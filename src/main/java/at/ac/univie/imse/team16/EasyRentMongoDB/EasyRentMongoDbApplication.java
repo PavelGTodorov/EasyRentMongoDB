eb871ae483a8cb41c62a4ec2c40fe478ecbf0465
Pavel Todorov <a1228046@unet.univie.ac.at>
2017-06-13 19:26:07 +0200

    Initial Commit

---
+++ src/main/java/at/ac/univie/imse/team16/EasyRentMongoDB/EasyRentMongoDbApplication.java
@@ -0,0 +1,12 @@
+package at.ac.univie.imse.team16.EasyRentMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyRentMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyRentMongoDbApplication.class, args);
+	}
+}
